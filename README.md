410656984c69fbc272af700587597a19e6d333ad
Michelle Conway <32267382+mconwa02@users.noreply.github.com>
2024-05-15 22:00:24 +0100

    Adding statsmodels (#3)

--- README.md
+++ README.md
@@ -4,6 +4,17 @@ MLOPS in Financial Services
 
 ![speaker_flyer.jpg](docs%2Fspeaker_flyer.jpg)
 
+### Statsmodels Example
+
+An example of how you can use the statsmodels library in Python to 
+build an ARIMA (AutoRegressive Integrated Moving Average) model for 
+time series forecasting:
+ 
+Make sure to replace 'your_data.csv' with the path to your actual data file. 
+Adjust the values of p, d, and q according to your data and model requirements.
+Also, consider whether differencing is necessary to make your data stationary,
+and modify the code accordingly.
+
 https://pypi.org/project/statsmodels/
 
 https://pypi.org/project/scikit-learn/
---
+++ src/models/sm_time_series.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from statsmodels.tsa.arima.model import ARIMA
+
+data = pd.read_csv(r"C:\dev\data\dsf\product_info_simple_final_train.csv")
+
+# Assuming your data has a 'date' column, set it as the index
+data["transaction_date"] = pd.to_datetime(data["transaction_date"])
+data = data.set_index("transaction_date")
+
+# Check if your data is stationary or not
+# If not, you may need to difference it to make it stationary
+stationary_data = data["apply_amt"].diff().dropna()
+
+# Plot the ACF and PACF to determine the order of AR and MA terms
+plot_acf(stationary_data)
+plot_pacf(stationary_data)
+plt.show()
+
+# Define the order of the ARIMA model (p, d, q)
+# p: number of autoregressive terms
+# d: number of differences
+# q: number of moving average terms
+p = 1  # Example value
+d = 1  # Example value
+q = 1  # Example value
+
+# Fit the ARIMA model
+model = ARIMA(data, order=(p, d, q))
+results = model.fit()
+
+# Forecast future values
+forecast_steps = 10  # Example value
+forecast = results.forecast(steps=forecast_steps)
+
+# Plot the original data and the forecast
+plt.plot(data.index, data.values, label="Original Data")
+plt.plot(
+    pd.date_range(start=data.index[-1], periods=forecast_steps + 1, freq="M")[1:],
+    forecast,
+    label="Forecast",
+)
+plt.xlabel("Date")
+plt.ylabel("Value")
+plt.title("ARIMA Forecast")
+plt.legend()
+plt.show()
