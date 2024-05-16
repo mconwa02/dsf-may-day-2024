04f06a056bda6168591bddd82d054dadcbe35094
mconwa02 <mconwa02@dcs.bbk.ac.uk>
2024-05-16 20:46:01 +0100

    working darts

--- src/models/dt_time_series.py
+++ src/models/dt_time_series.py
@@ -3,14 +3,22 @@ import pandas as pd
 from darts import TimeSeries
 from darts.models import ARIMA
 
-data = pd.read_csv(r'C:\dev\data\dsf\product_info_simple_final_train.csv')
+data = pd.read_csv(r"C:\dev\data\dsf\product_info_simple_final_train.csv")
 
 # Assuming your data has a 'date' column, set it as the index
-data['transaction_date'] = pd.to_datetime(data['transaction_date'])
-data.set_index('transaction_date', inplace=True)
+data["date"] = pd.to_datetime(data["transaction_date"])
+
+# pick one product with a least 30 values
+data = data[data["product_pid"] == "product100"]
 
 # Convert your pandas DataFrame into a Darts TimeSeries object
-series = TimeSeries.from_dataframe(data, 'transaction_date', 'total_net_value')
+series = TimeSeries.from_dataframe(
+    data,
+    time_col="transaction_date",
+    value_cols="apply_amt",
+    fill_missing_dates=True,
+    freq=1,
+)
 
 # Split the series into training and testing sets
 train_size = int(0.8 * len(series))
@@ -24,11 +32,10 @@ model.fit(train)
 forecast = model.predict(len(test))
 
 # Plot the original data and the forecast
-series.plot(label='Original Data')
-forecast.plot(label='Forecast', lw=2)
-plt.xlabel('Date')
-plt.ylabel('Total Net Value')
-plt.title('ARIMA Forecast')
+series.plot(label="Original Data")
+forecast.plot(label="Forecast", lw=2)
+plt.xlabel("Date")
+plt.ylabel("Total Amt")
+plt.title("ARIMA Forecast")
 plt.legend()
 plt.show()
-
