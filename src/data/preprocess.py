9af95477094941b24cdff5a44f1929747ef285d3
mconwa02 <mconwa02@dcs.bbk.ac.uk>
2024-05-16 22:36:11 +0100

    not work sk and sm

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -1,7 +1,18 @@
 import os
 import pandas as pd
 
+
 PATH = r"C:\dev\data\dsf"
-df = pd.read_csv(os.path.join(PATH, "product_info_simple_final_train.csv"))
-print(df.head())
-print(df.info())
+
+
+def process_data():
+    """Collecting data locally"""
+    df = pd.read_csv(os.path.join(PATH, "product_info_simple_final_train.csv"))
+    df["transaction_date"] = pd.to_datetime(df["transaction_date"])
+    df = df.set_index("transaction_date")
+    print(df.head())
+    print(df.info())
+
+
+if __name__ == "__main__":
+    process_data()
